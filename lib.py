3e934afa588faac0a072dbfc8f83d522f480a25a
Grace Valmadrid <>
2019-11-01 10:39:35 +0000

    Fri morning updates

--- lib.py
+++ lib.py
@@ -2,18 +2,37 @@
 
 
 
-from dotenv import load_dotenv
-import os
-load_dotenv()
+#from dotenv import load_dotenv
+#import os
+#load_dotenv()
+import requests
 
 class WeatherGetter():
-    def __init__(self):
-        # Let's set our secrets and keys from the .env file
-        # as environment variables.
-        self.BASE_URL = 'https://api.darksky.net'
-        self.token = os.getenv('DARKSKY')
-        if len(self.token) == 0:
-            raise ValueError('Missing API key!')
+    def __init__(self, date = None):
+        self.date = date + "T12:00:00"  #set default time to 12nn
+        self.lat = 52.52 #Berlin
+        self.long = 13.405 #Berlin
+        self.key = "dc40686a80b16d006b8747f0706e7215" #add key here
+        self.link = f"https://api.darksky.net/forecast/{self.key}/{self.lat},{self.long},{self.date}?exclude=currently,hourly,flags"
+        self.weather = self.getweathersummary()
+        
+    def getweathersummary(self):
+        r = requests.get(self.link)
+        response = r.json()
+        try:
+            response['daily']['data'][0]['icon'] 
+        except:
+            return response['daily']['data'][0]['summary']  #if there is no icon then get summary
+        else:
+            return response['daily']['data'][0]['icon']
+
+
+        
+#         self.BASE_URL = 'https://api.darksky.net'
+#         self.token = os.getenv('DARKSKY')
+#         if len(self.token) == 0:
+#             raise ValueError('Missing API key!')
+
 
 class MongoHandler():
     pass
